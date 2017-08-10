bd37ed08f130ae6cbd152f44ebe529d01fe9cbc3
samkong110 <shitaoxujiang@163.com>
2017-08-10 11:20:05 +0800

    20170810 早

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -10,9 +10,16 @@ def login_action(request):
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         if username == 'admin' and password == 'admin123':
-            return HttpResponse('login success!')
+            return HttpResponseRedirect('/event_manage/')
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
+    else:
+        return render(request,'index.html',{'error': 'username or password error!'})
+
+
+# 发布会管理
+def event_manage(request):
+    return render(request,"event_manage.html")
 
 
 
