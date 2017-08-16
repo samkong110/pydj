e05a07504a1603d4f1acff6f66eac69d76cff674
samkong110 <shitaoxujiang@163.com>
2017-08-16 13:24:39 +0800

    20170816  13:24

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -5,15 +5,13 @@ from django.contrib.auth.decorators import login_required
 from sign.models import Event, Guest
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-
-
-
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 
 # 登录动作
+@login_required
 def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
@@ -37,71 +35,7 @@ def event_manage(request):
     username = request.session.get('user', '')  # 读取浏览器 cookie
     return render(request, "event_manage.html", {"user": username, "events": event_list})
 
-
-def search_name(request):
-    username = request.session.get('user', '')
-    search_name = request.GET.get("name", "")
-    event_list = Event.objects.filter(name__contains=search_name)
-    return render(request, "event_manage.html", {"user": username, "events": event_list})
-
-
-# 嘉宾管理
 @login_required
-def guest_manage(request):
-    username = request.session.get('user', '')
-    guest_list = Guest.objects.all()
-    paginator = Paginator(guest_list, 2)
-    page = request.GET.get('page')
-    try:
-        contacts = paginator.page(page)
-    except PageNotAnInteger:
-        # If  page is not an integer, deliver first page.
-        contacts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        contacts = paginator.page(paginator.num_pages)
-    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
-
-
-from django.http import HttpResponseRedirect
-from django.shortcuts import render,get_object_or_404
-from django.contrib import auth
-from django.contrib.auth.decorators import login_required
-from sign.models import Event, Guest
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-
-
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
-
-
-# 登录动作
-def login_action(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            request.session['user'] = username  # 添加浏览器 cookie
-            response = HttpResponseRedirect('/event_manage/')
-            return response
-        else:
-            return render(request, 'index.html', {'error': 'username or password error!'})
-    else:
-        return render(request, 'index.html', {'error': 'username or password error!'})
-
-
-# 发布会管理
-@login_required
-def event_manage(request):
-    event_list = Event.objects.all()
-    username = request.session.get('user', '')  # 读取浏览器 cookie
-    return render(request, "event_manage.html", {"user": username, "events": event_list})
-
-
 def search_name(request):
     username = request.session.get('user', '')
     search_name = request.GET.get("name", "")
